bce3a4fce105ef8af32d238ed47bb8d5d84bb680
DaoLuoLTS <61882306+ShenHaiSu@users.noreply.github.com>
2025-09-22 21:34:48 +0800

    Initial commit

---
+++ frontEnd/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <div>
+      <span class="text-2xl bg-[#f0f0f0]">asdasd</span>
+      <Button label="asdasd" />
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import {Button} from "primevue";
+
+</script>
